049f168ace710e1b68218e261a80de7e65e51081
ViacheslavSukhov <vi.sukhov@ya.ru>
2021-01-19 23:42:39 +0300

    Fix extends adapter

--- features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/adapter/TabAdapter.kt
+++ features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/adapter/TabAdapter.kt
@@ -1,12 +1,15 @@
 package ru.smartlab.demo.marketmap.fragments.adapter
 
+import android.content.Context
 import androidx.fragment.app.FragmentManager
+import ru.smartlab.demo.marketmap.R
 import ru.smartlab.demo.marketmap.fragments.MapMarket
 import ru.smartlab.demo.marketmap.fragments.TableMarket
 import ru.smartlab.demo.marketmap.model.ExchangeInstrument
 import ru.smartlab.demo.tabs.BasePagerAdapter
 
-class TabAdapter(fm: FragmentManager) : BasePagerAdapter<ExchangeInstrument>(fm) {
+class TabAdapter(private val context: Context, fm: FragmentManager) :
+    BasePagerAdapter<ExchangeInstrument>(fm) {
 
     init {
 
@@ -22,8 +25,8 @@ class TabAdapter(fm: FragmentManager) : BasePagerAdapter<ExchangeInstrument>(fm)
         title = {
 
             when (it) {
-                0 -> ""
-                else -> ""
+                0 -> context.resources.getString(R.string.map)
+                else -> context.resources.getString(R.string.table)
             }
         }
     }
