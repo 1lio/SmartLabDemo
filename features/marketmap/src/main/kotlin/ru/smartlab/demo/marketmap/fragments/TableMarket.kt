1d9c732fdde0fda1380c0f603a124a206e97075b
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-20 15:27:26 +0300

    Bad table market implementation

--- features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/TableMarket.kt
+++ features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/TableMarket.kt
@@ -5,28 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.smartlab.demo.marketmap.R
 import ru.smartlab.demo.marketmap.fakeRepo.MoscowExchangeRepository
 import ru.smartlab.demo.marketmap.fakeRepo.MoscowExchangeRepository.StockGroup
+import ru.smartlab.demo.marketmap.fragments.adapter.TableAdapter
 
+// Demo Variant - not optimized (BAD VARIANT)
 class TableMarket : Fragment() {
 
     // Tmp
-    val listData = MoscowExchangeRepository().getListStocks()
+    private val listData = MoscowExchangeRepository().getListStocks()
 
-    val gazAndOil = listData.map { it.group == StockGroup.GAZ_AND_OIL }
-    val finance = listData.map { it.group == StockGroup.FINANCE }
-    val metallurgy = listData.map { it.group == StockGroup.METALLURGY }
-    val it = listData.map { it.group == StockGroup.IT }
-    val consumerSector = listData.map { it.group == StockGroup.CONSUMER_SECTOR }
-    val energy = listData.map { it.group == StockGroup.ENERGY }
-    val transport = listData.map { it.group == StockGroup.TRANSPORT }
-    val chemistry = listData.map { it.group == StockGroup.CHEMISTRY }
-    val agro = listData.map { it.group == StockGroup.AGRO }
-    val mechanicalEngineering = listData.map { it.group == StockGroup.MECHANICAL_ENGINEERING }
-    val other = listData.map { it.group == StockGroup.OTHER }
+    private val gazAndOilList = listData.filter { it.group == StockGroup.GAZ_AND_OIL }
+    private val financeList = listData.filter { it.group == StockGroup.FINANCE }
+    private val metallurgyList = listData.filter { it.group == StockGroup.METALLURGY }
+    private val itList = listData.filter { it.group == StockGroup.IT }
+    private val consumerSectorList = listData.filter { it.group == StockGroup.CONSUMER_SECTOR }
+    private val energyList = listData.filter { it.group == StockGroup.ENERGY }
+    private val transportList = listData.filter { it.group == StockGroup.TRANSPORT }
+    private val chemistryList = listData.filter { it.group == StockGroup.CHEMISTRY }
+    private val agroList = listData.filter { it.group == StockGroup.AGRO }
+    private val mechanicalEngineeringList =
+        listData.filter { it.group == StockGroup.MECHANICAL_ENGINEERING }
+    private val constructionList = listData.filter { it.group == StockGroup.CONSTRUCTION }
 
+    private val otherList = listData.filter { it.group == StockGroup.OTHER }
+
+    private lateinit var gazAndOilRecycler: RecyclerView
+    private lateinit var financeRecycler: RecyclerView
+    private lateinit var metallurgyRecycler: RecyclerView
+    private lateinit var itRecycler: RecyclerView
+    private lateinit var consumerSectorRecycler: RecyclerView
+    private lateinit var energyRecycler: RecyclerView
+    private lateinit var transportRecycler: RecyclerView
+    private lateinit var chemistryRecycler: RecyclerView
+    private lateinit var agroRecycler: RecyclerView
+    private lateinit var mechanicalEngineeringRecycler: RecyclerView
+    private lateinit var constructionRecycler: RecyclerView
+    private lateinit var otherRecycler: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,9 +55,54 @@ class TableMarket : Fragment() {
 
         val v = inflater.inflate(R.layout.table_fragment, container, false)
 
-        val recycler: RecyclerView = v.findViewById(R.id.tableRecycler)
+        gazAndOilRecycler = v.findViewById(R.id.recyclerGaz)
+        financeRecycler = v.findViewById(R.id.recyclerFinance)
+        metallurgyRecycler = v.findViewById(R.id.recyclerMetallurgy)
+        itRecycler = v.findViewById(R.id.recyclerIT)
+        consumerSectorRecycler = v.findViewById(R.id.recyclerConsumer)
+        energyRecycler = v.findViewById(R.id.recyclerEnergy)
+        transportRecycler = v.findViewById(R.id.recyclerTransport)
+        chemistryRecycler = v.findViewById(R.id.recyclerChemistry)
+        agroRecycler = v.findViewById(R.id.recyclerAgro)
+        mechanicalEngineeringRecycler = v.findViewById(R.id.recyclerMechanical)
+        constructionRecycler = v.findViewById(R.id.recyclerConstruction)
+        otherRecycler = v.findViewById(R.id.recyclerOther)
 
 
+        val groupRecycler = listOf(
+            gazAndOilRecycler,
+            financeRecycler,
+            metallurgyRecycler,
+            itRecycler,
+            consumerSectorRecycler,
+            energyRecycler,
+            transportRecycler,
+            chemistryRecycler,
+            agroRecycler,
+            mechanicalEngineeringRecycler,
+            constructionRecycler,
+            otherRecycler
+        )
+
+        val groupList = listOf(
+            gazAndOilList,
+            financeList,
+            metallurgyList,
+            itList,
+            consumerSectorList,
+            energyList,
+            transportList,
+            chemistryList,
+            agroList,
+            mechanicalEngineeringList,
+            constructionList,
+            otherList
+        )
+
+        groupRecycler.forEachIndexed { index, view ->
+            view.layoutManager = GridLayoutManager(context, 3)
+            view.adapter = TableAdapter(groupList[index])
+        }
 
         return v
     }
