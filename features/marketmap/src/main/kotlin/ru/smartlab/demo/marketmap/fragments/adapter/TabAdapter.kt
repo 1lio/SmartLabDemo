7625048273f68aa8dfeab888cd8edb4741fb0d15
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-20 09:29:27 +0300

    Fix MapMarket Adapter

--- features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/adapter/TabAdapter.kt
+++ features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/adapter/TabAdapter.kt
@@ -8,27 +8,12 @@ import ru.smartlab.demo.marketmap.fragments.TableMarket
 import ru.smartlab.demo.marketmap.model.ExchangeInstrument
 import ru.smartlab.demo.tabs.BasePagerAdapter
 
-class TabAdapter(private val context: Context, fm: FragmentManager) :
+class TabAdapter(context: Context, fm: FragmentManager) :
     BasePagerAdapter<ExchangeInstrument>(fm) {
 
     init {
-
-        countItems = 2
-
-        items = {
-            when (it) {
-                0 -> MapMarket()
-                else -> TableMarket()
-            }
-        }
-
-        title = {
-
-            when (it) {
-                0 -> context.resources.getString(R.string.map)
-                else -> context.resources.getString(R.string.table)
-            }
-        }
+        setFragments(listOf(MapMarket(), TableMarket()))
+        setTabTitles(listOf(context.resources.getString(R.string.map), context.resources.getString(R.string.table)))
     }
 
 }
