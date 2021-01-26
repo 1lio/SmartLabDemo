def02c1687c913a5f2a38abe3fcbc29218f06695
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-01-26 20:01:56 +0300

    add index instrument

--- base/core/src/main/kotlin/ru/smartlab/demo/core/entity/ExchangeInstrument.kt
+++ base/core/src/main/kotlin/ru/smartlab/demo/core/entity/ExchangeInstrument.kt
@@ -1,16 +1,17 @@
 package ru.smartlab.demo.core.entity
 
+import ru.smartlab.demo.core.enum.ExchangeInstrument
+
 data class ExchangeInstrument(
     val id: Long = -1,
 
-    val type: String = "stock",
+    val type: String = ExchangeInstrument.Type.STOCK,
     val name: String = "Gazprom",
     val rating: Int = 1,
     val ticket: String = "GAZP",
-    val group: String = "Oil and Gaz",
-
+    val group: String = ExchangeInstrument.Direction.GAZ_AND_OIL,
+    val index: List<String> = listOf(), // Индекс (IMOEX, MOEX10, IRTS, RTS2)
     val lastPrice: Float = 200f,
     val lastPriceChange: Float = -1.26f,
-
     val turnover: String = "14,4 млрд"
 )
