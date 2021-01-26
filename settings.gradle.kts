389984c9d094e2b1810358311e49ce80e6fadca3
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-26 12:28:42 +0300

    Added new addModule function

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,38 +1,39 @@
 rootProject.buildFileName = "build.gradle.kts"
 rootProject.name = "SmartLabDemo"
+val appID = "ru.smartlab.demo"
 
 // base
 include(":app")
 
-addCore("core")
-//addCore("navigation")
+addModule("core", isCore = true)
 
 // features
-addFeature("account")
-addFeature("chat")
-addFeature("feed")
-addFeature("splash")
-addFeature("marketmap")
-addFeature("stocklenta")
+addModule("account")
+addModule("chat")
+addModule("feed")
+addModule("splash")
+addModule("marketmap")
+addModule("stocklenta")
 
-addFeature("tabs")          // base tabs fragment
+addModule("tabs")          // base tabs fragment
+
+//tmp
+addModule("navigation", isCore = true)
 
 
-fun addFeature(moduleName: String) {
-    include(moduleName)
-    project(":$moduleName").projectDir = File(rootDir, "/features/$moduleName")
-}
 
-fun addCore(moduleName: String) {
+fun addModule(moduleName: String, isCore: Boolean = false) {
+
+    val moduleType = if (isCore) "base" else "features"
 
     // Check dir
-    val modulePath = "base/$moduleName"
+    val modulePath = "$moduleType/$moduleName"
     val moduleDir = File(rootDir, modulePath)
 
     if (!moduleDir.exists()) {
 
         // make Module Directory
-        moduleDir.mkdir()
+        moduleDir.mkdirs()
 
         // create build.gradle.kts file
         val gradleConfig = """
@@ -40,15 +41,33 @@ fun addCore(moduleName: String) {
                 implementation(Config.Libs.Androidx.appCompat)
                 implementation(Config.Libs.Androidx.core)
             }
-        """.trimIndent();
+        """.trimIndent()
 
-        val gradleFile = File( "//$moduleName/build.gradle.kts", gradleConfig)
-
-        gradleFile.createNewFile()
+        java.io.PrintWriter("$moduleType/$moduleName/build.gradle.kts").runCatching {
+            this.println(gradleConfig)
+            this.close()
+        }.onFailure {
+            it.printStackTrace()
+        }
 
         // make Package Directory
+        val packageID = appID.replace('.', '/')
+        File(rootDir, "$moduleType/$moduleName/src/main/kotlin/$packageID/$moduleName").mkdirs()
+
+        // add Manifest
+        val manifest = """<?xml version="1.0" encoding="utf-8"?>
+            |<manifest package="ru.smartlab.demo.$moduleName" />""".trimMargin()
+
+        java.io.PrintWriter("$moduleType/$moduleName/src/main/AndroidManifest.xml").runCatching {
+            this.println(manifest)
+            this.close()
+        }.onFailure {
+            it.printStackTrace()
+        }
+
     }
 
+    //include module
     include(moduleName)
     project(":$moduleName").projectDir = File(rootDir, modulePath)
 }
