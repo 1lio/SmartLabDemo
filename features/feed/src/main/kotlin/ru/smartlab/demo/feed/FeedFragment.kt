e5d1217c18db48654ac3696f383b576d502a4a58
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-26 16:14:21 +0300

    Moved parse implementation

--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/FeedFragment.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/FeedFragment.kt
@@ -9,18 +9,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
-import org.jsoup.Jsoup
-import org.jsoup.select.Elements
 import ru.smartlab.demo.feed.adapter.FeedAdapter
-import ru.smartlab.demo.feed.model.Feed
+import ru.smartlab.demo.feed.datastore.api.ParseService
 import ru.smartlab.demo.feed.row.RowType
 import ru.smartlab.demo.feed.row.TextRowType
-import kotlin.collections.ArrayList
 
 // FIX RECURSING ID'S IN ROWS
 
@@ -42,7 +36,7 @@ class FeedFragment : Fragment() {
         recyclerView.adapter = adapter
 
         GlobalScope.launch(Dispatchers.Main) {
-            initd().collect {
+            ParseService().subscribeBNews().collect {
                 items.add(TextRowType(it))
                 adapter.notifyItemChanged(items.size)
 
@@ -52,58 +46,4 @@ class FeedFragment : Fragment() {
         return v
     }
 
-
-    private suspend fun initd(): Flow<Feed> = flow {
-
-        val jsoupDocument = Jsoup
-            .connect("https://smart-lab.ru/")
-            .userAgent("Chrome/4.0.249.0 Safari/532.5")
-            .referrer("http://www.google.com")
-            .timeout(3000)
-            .get()
-
-        val elements: Elements = jsoupDocument.select(".topic")
-
-        for (e in elements) {
-
-            val title = e.select(".title ").text()
-            // val link = e.select(".title ").attr("abs:href")
-
-            val date = e.select(".date ").text()
-
-            val author = e.select(".author").text()
-
-            val likes = e.select(".total").text().let {
-                if (it.isNullOrEmpty()) 0 else it.toInt()
-            }
-
-            // Пока не получается корректно спарсить колличество просмотров
-            val views = e.select(".views-total").attr("abs:title").let {
-
-                if (it.isNullOrEmpty()) 0 else it.toInt()
-            }
-
-            val comments = e.select(".comments-total").select(".red").text().let {
-                if (it.isNullOrEmpty()) 0 else it.toInt()
-            }
-
-
-            val feed = Feed(
-                title = title,
-                content = "",
-                author = author,
-                date = date,
-                countViews = views,
-                countComments = comments,
-                countLikes = likes
-            )
-
-            emit(feed)
-        }
-
-    }.flowOn(Dispatchers.IO)
-
-    companion object {
-        const val TAG = "FEED_FRAGMENT"
-    }
 }
--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/datastore/api/ParseService.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/datastore/api/ParseService.kt
@@ -2,30 +2,85 @@ package ru.smartlab.demo.feed.datastore.api
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
+import ru.smartlab.demo.feed.model.Feed
+import java.io.IOException
 
 class ParseService {
 
     private val hashMap = mutableMapOf<String, String>()
 
 
-    init {
+    /* init {
 
-        GlobalScope.launch(Dispatchers.IO) {
+         GlobalScope.launch(Dispatchers.IO) {
 
-            val jsoupDocument = Jsoup.connect("https://smart-lab.ru/").get()
-            val elements: Elements = jsoupDocument.select(".title")
+             val jsoupDocument = Jsoup.connect("https://smart-lab.ru/").get()
+             val elements: Elements = jsoupDocument.select(".title")
 
-            for (e in elements) {
-                val element1 = e.select("a[href]")
-                hashMap[e.text()] = element1.attr("abs:href")
+             for (e in elements) {
+                 val element1 = e.select("a[href]")
+                 hashMap[e.text()] = element1.attr("abs:href")
+             }
+         }
+
+     }*/
+
+
+    suspend fun subscribeBNews(): Flow<Feed> = flow {
+
+        val jsoupDocument = Jsoup
+            .connect("https://smart-lab.ru/")
+            .referrer("http://www.google.com")
+            .timeout(3000)
+            .get()
+
+        val elements: Elements = jsoupDocument.select(".topic")
+
+        for (e in elements) {
+
+            val title = e.select(".title ").text()
+            // val link = e.select(".title ").attr("abs:href")
+
+            val date = e.select(".date ").text()
+
+            val author = e.select(".author").text()
+
+            val likes = e.select(".total").text().let {
+                if (it.isNullOrEmpty()) 0 else it.toInt()
             }
+
+            // Пока не получается корректно спарсить колличество просмотров
+            val views = e.select(".views-total").attr("abs:title").let {
+
+                if (it.isNullOrEmpty()) 0 else it.toInt()
+            }
+
+            val comments = e.select(".comments-total").select(".red").text().let {
+                if (it.isNullOrEmpty()) 0 else it.toInt()
+            }
+
+
+            val feed = Feed(
+                title = title,
+                content = "",
+                author = author,
+                date = date,
+                countViews = views,
+                countComments = comments,
+                countLikes = likes
+            )
+
+            emit(feed)
         }
 
-    }
+    }.flowOn(Dispatchers.IO)
 
 
     private suspend fun parseText(params: Array<String>): String {
