0a3a3aca36d0b628a5578808c719261a10454b0f
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-26 12:28:05 +0300

    Added jsoup and coroutines

--- features/feed/build.gradle.kts
+++ features/feed/build.gradle.kts
@@ -6,4 +6,9 @@ dependencies {
     implementation(Config.Libs.Androidx.material)
     implementation(Config.Libs.Androidx.vectorAnimation)
     implementation(Config.Libs.Androidx.constraint)
+
+    implementation(Config.Libs.Kotlin.coroutinesCore)
+    implementation(Config.Libs.Kotlin.coroutinesAndroid)
+
+    implementation(Config.Libs.Mics.jsoup)
 }
