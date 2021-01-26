3e4ae95e0d243ebdc081c712c48ec4a6ee5cc6e5
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-26 16:05:59 +0300

    Add base init

--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/model/Feed.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/model/Feed.kt
@@ -2,10 +2,10 @@ package ru.smartlab.demo.feed.model
 
 data class Feed(
 
-    val title: String,
-    val content: String,
-    val author: String,     // USE MODEL
-    val date: String,
+    val title: String = "",
+    val content: String = "",
+    val author: String = "",     // USE MODEL
+    val date: String = "",
 
     val countViews: Int = 0,
     val countComments: Int = 0,
