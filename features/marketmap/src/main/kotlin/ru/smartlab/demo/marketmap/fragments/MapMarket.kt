62a7ff1b972e278bd60dec504673197d40e5530d
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-01-26 22:58:10 +0300

    Started

--- features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/MapMarket.kt
+++ features/marketmap/src/main/kotlin/ru/smartlab/demo/marketmap/fragments/MapMarket.kt
@@ -1,11 +1,13 @@
 package ru.smartlab.demo.marketmap.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridLayout
 import androidx.fragment.app.Fragment
+import ru.smartlab.demo.core.enum.ExInstrument
 import ru.smartlab.demo.marketmap.fakeRepo.MoscowExchangeRepository
 
 class MapMarket : Fragment() {
@@ -22,10 +24,21 @@ class MapMarket : Fragment() {
 
     private fun getStockWidth() {
 
+        // Считаем общую сумму оборотов. Пусть с потерей точности (100%)
+        val allTurnover =
+            MoscowExchangeRepository().getStockList { it.group == ExInstrument.Direction.AGRO }
+                .sumOf { it.turnover.toInt() }
+        Log.i(TAG, "getStockWidth: $allTurnover")
 
-        // Считаем общую сумму оборотов. Пусть с потерей точности
-        MoscowExchangeRepository().getStockList().sumBy { it.turnover.toInt() }
+        // Расчитываем сколько каждая отрасль занимает %
 
 
+        // Расчитываем сколько конкретно акция занимает процентов
+
+
+    }
+
+    companion object {
+        const val TAG = "MAP_MARKET"
     }
 }
