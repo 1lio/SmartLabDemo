091f7b6c521c4028c56d7f5f89638808e4c49db3
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-03 19:04:46 +0300

    Fix numbers

--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedType.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedType.kt
@@ -2,9 +2,9 @@ package ru.smartlab.demo.feed.fragments
 
 object FeedType {
     const val FEED = 0
-    const val FEED_ALL = 2
-    const val FEED_TOP_DAY = 3
-    const val FEED_TOP_WEEK = 4
-    const val FEED_TOP_MOUTH = 5
-    const val FEED_TOP_ALL = 6
+    const val FEED_ALL = 1
+    const val FEED_TOP_DAY = 2
+    const val FEED_TOP_WEEK = 3
+    const val FEED_TOP_MOUTH = 4
+    const val FEED_TOP_ALL = 5
 }
