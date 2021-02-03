09a9ae33edf522dc702c4b7e180bf06089893e0d
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-03 19:06:26 +0300

    Fix feedFragment

--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedFragment.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedFragment.kt
@@ -9,46 +9,62 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import ru.smartlab.demo.feed.R
 import ru.smartlab.demo.feed.adapter.FeedAdapter
 import ru.smartlab.demo.feed.row.RowType
 import ru.smartlab.demo.feed.row.TextRowType
-import ru.smartlab.demo.network.impl.SmartLabRetrofitImpl
+import ru.smartlab.demo.network.api.SmartLabApi.FeedTop
+import ru.smartlab.demo.network.impl.SmartLabParserImpl
 
-// FIX RECURSING ID'S IN ROWS
-
+// Данный фрагмент отражет ленту новостей
 class FeedFragment : Fragment() {
-
-    lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: FeedAdapter
-    var items: ArrayList<RowType> = arrayListOf()
 
     override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? {
         super.onCreateView(inflater, group, state)
 
         val v = inflater.inflate(R.layout.feed_fragment, group, false)
+        val items: ArrayList<RowType> = arrayListOf()
+        val recyclerView: RecyclerView = v.findViewById(R.id.feedRecycler)
 
-        recyclerView = v.findViewById(R.id.feedRecycler)
         recyclerView.layoutManager = (LinearLayoutManager(group!!.context))
 
-        adapter = FeedAdapter(items)
+        val adapter = FeedAdapter(items)
         recyclerView.adapter = adapter
 
-        val repository = SmartLabRetrofitImpl()
+        val repository = SmartLabParserImpl()
 
         GlobalScope.launch(Dispatchers.Main) {
 
-            val resp = repository.getFeed()
-
-            resp.collection.forEach {
-                items.add(TextRowType(it.convertToTopic()))
-                adapter.notifyItemChanged(items.size)
+            val feedFlow = when (feedType) {
+                FeedType.FEED -> repository.getFeed()
+                FeedType.FEED_ALL -> repository.getFeedAll()
+                FeedType.FEED_TOP_DAY -> repository.getFeedTop(FeedTop.TOP_DAY)
+                FeedType.FEED_TOP_WEEK -> repository.getFeedTop(FeedTop.TOP_WEEK)
+                FeedType.FEED_TOP_MOUTH -> repository.getFeedTop(FeedTop.TOP_MOUNT)
+                FeedType.FEED_TOP_ALL -> repository.getFeedTop(FeedTop.TOP_ALL)
+                else -> repository.getFeed()
             }
 
+            feedFlow.collect {
+                it.forEach { topic ->
+                    // TODO: Определить тип ячейки
+                    items.add(TextRowType(topic))
+                    adapter.notifyItemChanged(items.size)
+                }
+            }
         }
 
         return v
     }
 
+    companion object {
+
+        private var feedType = 0 // 0-главная 1-все 2день 3-7 дней 4 -месяц 5 завсе время
+
+        fun setType(typeFeed: Int) {
+            feedType = typeFeed
+        }
+    }
 }
