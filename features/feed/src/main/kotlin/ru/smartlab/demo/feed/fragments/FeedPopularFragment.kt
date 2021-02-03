a75f8dc39a5ccd5ff6ef93b065e3e5eec3c05dd3
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-03 19:25:08 +0300

    Added Popular fragment

---
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedPopularFragment.kt
@@ -0,0 +1,27 @@
+package ru.smartlab.demo.feed.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import ru.smartlab.demo.feed.R
+import ru.smartlab.demo.feed.adapter.TabAdapterFeedTop
+
+class FeedPopularFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? {
+        super.onCreateView(inflater, group, state)
+
+        val v = inflater.inflate(R.layout.fr_tab, group, false)
+
+        val pager: ViewPager = v.findViewById(R.id.viewPager)
+        pager.adapter = TabAdapterFeedTop(requireContext(), childFragmentManager)
+
+        val tabLayout: TabLayout = v.findViewById(R.id.tabLayout)
+        tabLayout.post { tabLayout.setupWithViewPager(pager) }
+
+        return v
+    }
+}
