d7e69e121a285ed4f645becfab27f3996e2cea0e
ViacheslavSukhov <svo_efremov@mail.ru>
2021-02-03 14:42:35 +0300

    Minor

--- base/core/src/main/kotlin/ru/smartlab/demo/core/entity/Topic.kt
+++ base/core/src/main/kotlin/ru/smartlab/demo/core/entity/Topic.kt
@@ -1,13 +1,11 @@
 package ru.smartlab.demo.core.entity
 
-data class Topic(
+class Topic(
 
     val title: String = "",
     val content: String = "",
-
     val author: User = User("default"),
     val date: String = "",
-
     val countViews: Int = 0,
     val countComments: Int = 0,
     val countLikes: Int = 0
