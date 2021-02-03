ab89994a84e862e11f31f2724a8b8c2499b7b01d
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-03 19:25:24 +0300

    Added impl tabs

--- features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedMainFragment.kt
+++ features/feed/src/main/kotlin/ru/smartlab/demo/feed/fragments/FeedMainFragment.kt
@@ -1,6 +1,29 @@
 package ru.smartlab.demo.feed.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import ru.smartlab.demo.feed.R
+import ru.smartlab.demo.feed.adapter.TabAdapterFeed
 
+// Основной фрагмент для новостей включает три таба (Главная, Все подряд, лучшие)
 class FeedMainFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? {
+        super.onCreateView(inflater, group, state)
+
+        val v = inflater.inflate(R.layout.fr_tab, group, false)
+
+        val pager: ViewPager = v.findViewById(R.id.viewPager)
+        pager.adapter = TabAdapterFeed(requireContext(), childFragmentManager)
+
+        val tabLayout: TabLayout = v.findViewById(R.id.tabLayout)
+        tabLayout.post { tabLayout.setupWithViewPager(pager) }
+
+        return v
+    }
 }
