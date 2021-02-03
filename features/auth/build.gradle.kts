582b95066bd5b29ebf4914ed34c5501dec92c945
ViacheslavSukhov <svo_efremov@mail.ru>
2021-02-03 10:08:36 +0300

    Added constraint

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -3,7 +3,8 @@ dependencies {
     implementation(Config.Libs.Androidx.appCompat)
     implementation(Config.Libs.Androidx.core)
     implementation(Config.Libs.Androidx.material)
-    
+    implementation(Config.Libs.Androidx.constraint)
+
     // modules
     api(project(":core"))
  //   api(project(":network"))
