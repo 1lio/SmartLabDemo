ee86708b7b929808764b7fd09edb77407b81e9ef
ViacheslavSukhov <svo_efremov@mail.ru>
2021-02-04 10:47:14 +0300

    Used Base url site

--- base/core/src/main/kotlin/ru/smartlab/demo/core/utils/Constants.kt
+++ base/core/src/main/kotlin/ru/smartlab/demo/core/utils/Constants.kt
@@ -3,5 +3,6 @@ package ru.smartlab.demo.core.utils
 object Constants {
 
     const val BASE_URL = "https://smart-lab.ru/read" // This url used official application
+    const val BASE_SITE = "https://smart-lab.ru/"    // Official site
 
 }
